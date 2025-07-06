4c78a59e2e3dab732eafa61aad53afcbfb603e0d
hauptmann <7034052@gmail.com>
2025-07-07 01:04:38 +0300

    Добавил logger.py

--- db_init.py
+++ db_init.py
@@ -1,28 +1,23 @@
 import mysql.connector as mysqlconn
 from config import *
 
-
-def sql_query(query: str) -> str:
-    lst = [i.split() for i in query.split(';')]
-    print(lst)
-
-
-
-content = ''
-try:
-    with open(SQL_FILE_NAME, 'r') as file:
-        content = file.read()
-        command = content
-        print(content)
-except FileNotFoundError as e:
-        print(e)
-
-with mysqlconn.connect(host=HOST_IP,
-                       user=USER_NAME,
-                       password=PASSWORD) as connection:
+def db_init() -> None:
+    content = ''
     try:
-            with connection.cursor() as cursor:
-                cursor.execute(command)
+        with open(SQL_FILE_NAME, 'r') as file:
+            content = file.read()
+            command = content
+            #print(content)
+    except FileNotFoundError as e:
+            print(e)
 
+    connection = mysqlconn.connect(host=HOST_IP,
+                           user=USER_NAME,
+                           password=PASSWORD)
+    try:
+        cursor = connection.cursor()
+        cursor.execute(command)
     except mysqlconn.Error as e:
         print(e)
+
+
